abbde7ee559e45c15438647b6cd6ba6eb7c1f4aa
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2017-08-26 14:28:00 -0500

    added user model, added toolbar for users on conditional in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
       invoices_path
     end  
     
+    def after_sign_in_path_for(users)
+      accounts_path
+    end  
+
      protected
 
     def logged_in
